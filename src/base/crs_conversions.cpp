372412b3f307c0074e4b4b8d06632e9664936c65
ricardcd <ricardcd@gmail.com>
2019-03-04 17:45:58 +0100

    More precise conversion from ecef to lat/lon/height coordinates.

--- src/base/crs_conversions.cpp
+++ src/base/crs_conversions.cpp
@@ -43,21 +43,72 @@ void llh2ecef( const double& lat, const double& lon, const double& h,
 void ecef2llh( const double& x, const double& y, const double& z,
                double& lat, double& lon, double& h )
 {
-    // Constants for the WGS84 Ellipsoid
-    const double wgs84_a = 6378137.0;               // Semi-major axis
-    const double wgs84_a2 = wgs84_a*wgs84_a;        // Semi-major axis squared
-    const double wgs84_e2 = 6.6943799901377997e-3;  // First eccentricity squared
-    const double wgs84_b = 6356752.3142451793;      // Semi-minor axis
-    const double wgs84_b2 = wgs84_b*wgs84_b;        // Semi-minor axis squared
+    // --- Simple method ---
+//    // Constants for the WGS84 Ellipsoid
+//    const double wgs84_a = 6378137.0;               // Semi-major axis
+//    const double wgs84_a2 = wgs84_a*wgs84_a;        // Semi-major axis squared
+//    const double wgs84_e2 = 6.6943799901377997e-3;  // First eccentricity squared
+//    const double wgs84_b = 6356752.3142451793;      // Semi-minor axis
+//    const double wgs84_b2 = wgs84_b*wgs84_b;        // Semi-minor axis squared
+//
+//    double ep = sqrt((wgs84_a2 - wgs84_b2)/wgs84_b2);
+//    double p = sqrt(x*x + y*y);
+//    double th = atan2(wgs84_a*z, wgs84_b*p);
+//    lon = atan2(y, x);
+//    lat = atan2( z + (ep*ep) * wgs84_b * (sin(th)*sin(th)*sin(th)),
+//                 p - wgs84_e2 * wgs84_a * (cos(th)*cos(th)*cos(th)) );
+//    double N = wgs84_a/sqrt(1 - wgs84_e2 * (sin(lat)*sin(lat)));
+//    h = (p/cos(lat)) - N;
 
-    double ep = sqrt((wgs84_a2 - wgs84_b2)/wgs84_b2);
-    double p = sqrt(x*x + y*y);
-    double th = atan2(wgs84_a*z, wgs84_b*p);
-    lon = atan2(y, x);
-    lat = atan2( z + (ep*ep) * wgs84_b * (sin(th)*sin(th)*sin(th)),
-                 p - wgs84_e2 * wgs84_a * (cos(th)*cos(th)*cos(th)) );
-    double N = wgs84_a/sqrt(1 - wgs84_e2 * (sin(lat)*sin(lat)));
-    h = (p/cos(lat)) - N;
+    // --- Olson ---
+    // Implements: (Olson, D. K. (1996). "Converting earth-Centered, Earth-Fixed Coordinates to Geodetic Coordinates," IEEE Transactions on Aerospace and Electronic Systems, Vol. 32, No. 1, January 1996, pp. 473-476
+    // Code extracted from: http://danceswithcode.net/engineeringnotes/geodetic_to_ecef/geodetic_to_ecef.html
+
+    const double  a = 6378137.0;              //WGS-84 semi-major axis
+    const double e2 = 6.6943799901377997e-3;  //WGS-84 first eccentricity squared
+    const double a1 = 4.2697672707157535e+4;  //a1 = a*e2
+    const double a2 = 1.8230912546075455e+9;  //a2 = a1*a1
+    const double a3 = 1.4291722289812413e+2;  //a3 = a1*e2/2
+    const double a4 = 4.5577281365188637e+9;  //a4 = 2.5*a2
+    const double a5 = 4.2840589930055659e+4;  //a5 = a1+a3
+    const double a6 = 9.9330562000986220e-1;  //a6 = 1-e2
+
+    double zp = fabs(z);
+    double w2 = x*x + y*y;
+    double w = sqrt(w2);
+    double r2 = w2 + z*z;
+    double r = sqrt( r2 );
+    lon = atan2( y, x );       //Lon (final)
+    double s2 = z*z/r2;
+    double c2 = w2/r2;
+    double u = a2/r;
+    double v = a3 - a4/r;
+    double c, ss, s;
+    if( c2 > 0.3 ){
+        s = ( zp/r )*( 1.0 + c2*( a1 + u + s2*v )/r );
+        lat = asin( s );      //Lat
+        double ss = s*s;
+        c = sqrt( 1.0 - ss );
+    }
+    else{
+        c = ( w/r )*( 1.0 - s2*( a5 - u - c2*v )/r );
+        lat = acos( c );      //Lat
+        ss = 1.0 - c*c;
+        s = sqrt( ss );
+    }
+    double g = 1.0 - e2*ss;
+    double rg = a/sqrt( g );
+    double rf = a6*rg;
+    u = w - rg*c;
+    v = zp - rf*s;
+    double f = c*u + s*v;
+    double m = c*v - s*u;
+    double p = m/( rf/g + f );
+    lat = lat + p;      //Lat
+    h = f + m*p/2.0;     //Altitude
+    if( z < 0.0 ){
+        lat *= -1.0;     //Lat
+    }
 
     // Radians to degrees
     lon *= 180./M_PI;
